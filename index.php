06089861463fc74a79aa19e2d9354f15d18444d5
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2021-10-12 05:26:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="title" content="" />
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta name="robots" content="index, follow" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="language" content="English" />
+
+  <link rel="icon" href="https://i.im.ge/2021/09/18/TPqHLL.png">
+  <title>Sherif Abdullah Mahmoud</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+  <style>
+    body{
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+}
+.bi .bi-github{color:white;border-color:#222}
+.bi .bi-github{color:white;background-color:#6441a5;border-color:#6441a5}
+.bi .bi-github:focus+.bi .bi-github,.bi .bi-github:focus{color:#6441a5;}
+</style>
+
+</head>
+
+<body class="bg-light">
+  <div class="card bg-white border-0 mb-5 shadow-sm">
+    <div class="card-body d-flex align-items-center justify-content-start p-5 flex-sm-column flex-column flex-md-row">
+      <img src="https://i.im.ge/2021/09/18/TPqHLL.png" class="rounded-circle bg-secondary mb-sm-4 mb-4 mb-md-0" alt="profile" width="200" />
+      <div class="
+            d-flex
+            flex-column
+            align-items-center
+            align-items-sm-center
+            align-items-md-start
+            justify-content
+            between
+            ms-0
+            ms-md-4
+            mb-sm-5
+            mb-5
+            mb-md-0
+          ">
+        <h1 class="display-4 fw-bold mb-0">Sherif Abdullah</h1>
+        <p class="fs-3 fw-normal">Developer</p>
+        <div class="d-flex align-items-center justify-content-between" >
+            <a   href="https://github.com/sherif-abdallah" >
+          <button class="btn btn-primary rounded-circle ms-2 fs-4 p-0" style="height: 40px; width: 40px; background-color: #222;">
+            <i class="bi bi-github" id="github" ></i>
+          </button>
+          </a>
+          <a   href="//facebook.com/profile.php?id=100068549361726">
+          <button class="btn btn-primary rounded-circle ms-2 fs-4 p-0" style="height: 40px; width: 40px;">
+            <i class="bi bi-facebook"></i>
+          </button>
+        </a>
+        <a   href="//mail.google.com/mail/u/0/?fs=1&to=sherifabdalla2005@gmail.com&su=&body=&bcc=&tf=cm">
+          <button class="btn btn-info text-white rounded-circle ms-2 fs-4 p-0" style="height: 40px; width: 40px;">
+            <i class="bi bi-envelope-fill"></i>
+          </button>
+        </a>
+        </div>
+      </div>
+      <div class="flex-fill d-flex align-items-center justify-content-center justify-content-md-end">
+        <a href="//messenger.com/t/100068549361726/"  >
+        <button class="btn btn-primary btn-lg">
+          <i class="bi bi-person-lines-fill me-3"></i>Contact Me
+        </button>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container h-100">
+    <div class="row pb-5">
+      <div class="col-12 col-lg-8 overflow-auto">
+        <div class="card rounded border-0 mb-3 shadow-sm p-3">
+          <div class="card-body">
+            <h2 class="card-title fs-1 mb-5 fw-bold">About Me</h2>
+            <p class="card-text">
+            I am a single young man. I like watching science fiction movies. My favorite sports are swimming and basketball.
+            I started driving lessons.
+            I am a hard working, honest individual. I am a good timekeeper, always willing to learn new skills. I am friendly, helpful and polite, have a good sense of humour. I am able to work independently in busy environments and also within a team setting. I am outgoing and tactful, and able to listen effectively when solving problems.
+            </p>
+          </div>
+        </div>
+
+        <div class="card border-0 rounded shadow-sm mb-3 p-3">
+          <div class="card-body">
+            <h2 class="card-title mb-5 fs-1 fw-bold">Projects</h2>
+            <div>
+              <div class="row">
+                <div class="col-md-8">
+                    <ul>
+                  <p>
+                  <li>  <a   href="http://sherif.rf.gd/yt-adblock/">YouTube Ad blocker</a>
+                  </p>
+                  <p>
+                  <li>  <a   href="http://sherif.rf.gd/code_now/">HTML Code Editor</a>
+                  </p>
+                  <p>
+                   <li> <a   href="http://sherif.rf.gd/search_engine/">Search Engine</a>
+                  </p>
+                   <p>
+                    <li> <a   href="https://streamable.com/e/1k86kd">Canspect</a>
+                   </p>
+                   <p>
+                    <li> <a   href="https://streamable.com/e/hssuse">YouTube Downloader</a>
+                   </p>
+                    <p>
+                    <li> <a  href="http://sherif.rf.gd/weather/">Weather Condition</a>
+                   </p>
+                    <p>
+                    <li> <a  href="http://4muslims.epizy.com/">4muslims</a>
+                   </p>
+                </ul>
+                </div>
+                <hr class="my-4 bg-secondary" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card border-0 rounded shadow-sm mb-3 p-3">
+          <div class="card-body">
+            <h2 class="card-title mb-5 fs-1 fw-bold">Skills</h2>
+            <div>
+              <div class="row">
+                <div class="col-md-8">
+                    <ul>
+                    <p>
+                    <li>HTML
+                    </p>
+                    <p>
+                    <li>CSS
+                    </p>
+                    <p>
+                    <li>JavaScript
+                    </p>
+                    <p>
+                    <li>Python
+                    </p>
+                    <p>
+                    <li>PHP
+                    </p>
+                    <p>
+                      <li>Git & Github
+                      </p>
+                    </ul>
+                </div>
+                <hr class="my-4 bg-secondary" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-0 col-lg-4 overflow-auto">
+        <div class="card border-0 rounded shadow-sm mb-3 p-3">
+          <div class="card-body">
+            <div class="
+                  d-flex
+                  flex-column
+                  align-items-start
+                  justify-content-between
+                ">
+              <div class="d-flex align-items-center justify-content-center mb-2">
+                <i class="bi bi-geo-alt-fill text-danger"></i>
+                <span class="ms-2">Egypt, Cairo</span>
+              </div>
+              <div class="d-flex align-items-center justify-content-center mb-2">
+                <i class="bi bi-envelope-fill text-primary"></i>
+                <a   href="//mail.google.com/mail/u/0/?fs=1&to=sherifabdalla2005@gmail.com&su=&body=&bcc=&tf=cm" class="ms-2 text-decoration-none text-dark">sherifabdalla2005@gmail.com</a>
+              </div>
+              <div class="d-flex align-items-center justify-content-center mb-2">
+                <i class="bi bi-telephone-fill text-success"></i>
+                <a    href="//api.whatsapp.com/send/?phone=%2B201061403772&text&app_absent=0" class="ms-2 text-decoration-none text-dark">+201061403772</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card border-0 rounded shadow-sm mb-3 p-3">
+          <div class="card-body">
+            <h2 class="card-title fs-3 fw-bold mb-4">Certification</h2>
+            <div class="mb-5">
+              <div>
+                <div class="border-start border-5 border-primary ps-4">
+                  <p>
+                      <a   href="cirtifications\1MAC-Compelete-Full-Stack-Devolopment-Track.pdf">1MAC</a> <br>
+                      <a   href="cirtifications\1MAC-CompleteFront-End-Development-Track.pdf">1MAC</a> <br>
+                      <a   href="cirtifications\Egypt-Fwd-Web-Devolopment-Challange.pdf">Egfwd</a> <br>
+                      <a   href="cirtifications\CertificateOfCompletion_CSS Essential Training.pdf">Linkedin</a> <br>
+                      <a   href="cirtifications\CertificateOfCompletion_HTML Essential Training.pdf">Linkedin</a> <br>
+                      <a   href="cirtifications\CertificateOfCompletion_JavaScript Essential Training 2017.pdf">Linkedin</a> <br>
+
+                    </p>
+                </div>
+                <div class="ps-4 border-start border-5 border-white">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card border-0 rounded shadow-sm mb-3 p-3">
+          <div class="card-body">
+            <h2 class="card-title fs-3 fw-bold mb-4">Education</h2>
+            <div class="d-flex align-items-center justify-content-start mb-3">
+              <div class="d-flex align-items-center justify-content-center">
+                <i class="m-0 p-0 bi bi-building"></i>
+              </div>
+              <div class="
+                    d-flex
+                    flex-column
+                    align-items-start
+                    justify-content-center
+                    ms-3
+                  ">
+                <p class="fw-bold text-secondary mb-0">School</p>
+                <p class="text-muted">Future 15 (2009 - Current)</p>
+              </div>
+            </div>
+
+            <div class="d-flex align-items-center justify-content-start mb-3">
+                <div class="d-flex align-items-center justify-content-center">
+                  <i class="m-0 p-0 bi bi-building"></i>
+                </div>
+                <div class="
+                      d-flex
+                      flex-column
+                      align-items-start
+                      justify-content-center
+                      ms-3
+                    ">
+                  <p class="fw-bold text-secondary mb-0">College</p>
+                  <a   href="https://egfwd.com/"></p>egfwd.com (2018 - 2019)</p></a>
+                </div>
+              </div>
+              <div class="d-flex align-items-center justify-content-start mb-3">
+                <div class="d-flex align-items-center justify-content-center">
+                  <i class="m-0 p-0 bi bi-building"></i>
+                </div>
+                <div class="
+                      d-flex
+                      flex-column
+                      align-items-start
+                      justify-content-center
+                      ms-3
+                    ">
+                  <p class="fw-bold text-secondary mb-0">College</p>
+                  <a   href="https://www.arabcoders.ae/"></p>arabcoders.ae (2019 - 2020)</p></a>
+                </div>
+              </div>
+              <div class="d-flex align-items-center justify-content-start mb-3">
+                <div class="d-flex align-items-center justify-content-center">
+                  <i class="m-0 p-0 bi bi-building"></i>
+                </div>
+                <div class="
+                      d-flex
+                      flex-column
+                      align-items-start
+                      justify-content-center
+                      ms-3
+                    ">
+                  <p class="fw-bold text-secondary mb-0">College</p>
+                  <a   href="https://elzero.org/"></p>elzero.org (2020 - 2020)</p></a>
+                </div>
+              </div>
+              <div class="d-flex align-items-center justify-content-start mb-3">
+                <div class="d-flex align-items-center justify-content-center">
+                  <i class="m-0 p-0 bi bi-building"></i>
+                </div>
+                <div class="
+                      d-flex
+                      flex-column
+                      align-items-start
+                      justify-content-center
+                      ms-3
+                    ">
+                  <p class="fw-bold text-secondary mb-0">College</p>
+                  <a   href="https://teracourses.com/"></p>teracourses.com (2020 - 2021)</p></a>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="card border-0 rounded shadow-sm p-3">
+          <div class="card-body">
+            <h2 class="card-title fs-3 fw-bold mb-4">Languages</h2>
+            <div class="
+                  d-flex
+                  flex-column
+                  align-items-start
+                  justify-content-center
+                  mb-3
+                ">
+              <p class="text-muted mb-1">
+                <span class="fw-bold text-secondary">Arabic: </span>Mother tongue
+              </p>
+              <div class="
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                    text-warning
+                  ">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill ms-1"></i>
+                <i class="bi bi-star-fill ms-1"></i>
+                <i class="bi bi-star-fill ms-1"></i>
+                <i class="bi bi-star-fill ms-1"></i>
+              </div>
+              <p class="text-muted mb-1">
+                <span class="fw-bold text-secondary">English: </span>Proficient
+              </p>
+              <div class="
+                    d-flex
+                    align-items-center
+                    justify-content-center
+                    text-warning
+                  ">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill ms-1"></i>
+                <i class="bi bi-star-fill ms-1"></i>
+                <i class="bi bi-star-fill ms-1"></i>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+
+</html>
